04a395929e2c9017626091d62a5f122c72f7c39d
AqiqiySalimuzzaakiy <aqiqiy.salimuzzaakiy@mhs.unsoed.ac.id>
2026-07-11 06:57:56 +0700

    Tambah halaman anggota di frontend

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import sqlite3
+from datetime import date, timedelta
 
 app = Flask(__name__)
+CORS(app)
 DB_NAME = 'perpustakaan.db'
 
 def get_db_connection():
